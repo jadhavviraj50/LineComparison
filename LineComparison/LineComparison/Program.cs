11a61f99713410309a1544d1107c85f5cc8a45eb
Viraj <jadhavviraj50@gmail.com>
2022-04-20 21:16:47 +0530

    [Viraj]-Added UC2Equalityof2Length

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -23,6 +23,29 @@ namespace LineComparisonComputation
                 double result = Math.Sqrt(temp1 + temp2);
                 return result;
             }
+            //UC2
+            Console.WriteLine("Please enter p and q coordinates ");
+            double p = Convert.ToDouble(Console.ReadLine());
+            double q = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter r and s coordinates ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double s = Convert.ToDouble(Console.ReadLine());
+            double finalResult2 = Distance(p, r, q, s);
+            Console.WriteLine("Distance between {0},{1} and {2},{3} is {4:F}", p, q, r, s, finalResult2);
+            int res = finalResult.CompareTo(finalResult2);
+            Console.WriteLine(res);
+            if (res == 0)
+            {
+                Console.WriteLine("Lengths are equal!");
+            }
+            if (res > 0)
+            {
+                Console.WriteLine("Line 1 is greater than line 2 ");
+            }
+            else
+            {
+                Console.WriteLine("Line 2 is greater than line 1 ");
+            }
         }
     }
 }
